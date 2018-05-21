c3bcc9db43b0b37fc8aa72e2318d3d9f350c2bae
ryksu <cv.alexisjesus@gmail.com>
2018-05-22 01:54:33 +0200

    comentar is coming

--- modulos/comentarios.php
+++ modulos/comentarios.php
@@ -1,7 +1,24 @@
 <?php if (isset($_SESSION['usuario'])&&!empty($_SESSION['usuario'])): ?>
+  <div class="c-comentar">
+    <div class="comentar">
+      <textarea name="texto"></textarea>
+    </div>
+    <div hidden id="c-enviar">
+      <button type="reset" name="Deshacer">
+        <span class="fas fa-redo-alt"></span>
+        Deshacer
+      </button>
+      <button type="submit" name="Enviar" id="Enviar">
+        <span class="far fa-paper-plane"></span>
+        Enviar
+      </button>
+    </div>
+  </div>
 <div class="c-comentarios">
   <h2>Comentarios</h2>
 </div>
+
+
 <?php  include_once 'paginacion.php'; ?>
 <script src="../../js/ObtenerComentarios.js"></script>
 <?php endif; ?>
