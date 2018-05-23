6df197834d3be170123b2c4d5d9f1595c4a78678
ryksu <cv.alexisjesus@gmail.com>
2018-05-24 00:53:30 +0200

    fix

--- Controlador/ObtenerComentarios.php
+++ Controlador/ObtenerComentarios.php
@@ -4,33 +4,18 @@ require_once '../Conf/Comentarios.php';
 $comentarios = new Comentarios();
 
 $nfilas = $comentarios->comentarioFila($_GET['id']);
-<<<<<<< HEAD
-$limite = 0;
-$pagina = 0;
-=======
->>>>>>> ab3e0b3f9bca0c53d4c1e0f1958dff10e5c482e3
 if ($nfilas>0) {
   $limite = 5;
   $pagina = 1;
   if (isset($_GET['pg'])) {
     $pagina = $_GET['pg'];
   }
-<<<<<<< HEAD
 $inicio = ($pagina-1)*$limite;
 $pagina_total = ceil($nfilas/$limite);
 $LIMIT = " LIMIT $inicio,$limite";
 
   if ($_GET['action']=="obtener") {
-
     $resultado = $comentarios->getComentarios($_GET['id'],$LIMIT);
-=======
-}
-$inicio = ($pagina-1)*$limite;
-$pagina_total = ceil($nfilas/$limite);
-
-  if ($_GET['action']=="obtener") {
-    $resultado = $comentarios->getComentarios($_GET['id'],$inicio,$limite);
->>>>>>> ab3e0b3f9bca0c53d4c1e0f1958dff10e5c482e3
     header("Content-Type: application/json; charset=UTF-8");
     sleep(2);
     echo $resultado;
@@ -38,7 +23,6 @@ $pagina_total = ceil($nfilas/$limite);
 
   if ($_GET['action']=="insertar") {
     $comentarios->insertarComentario($_GET['id'],$_SESSION['usuario'],$_GET['texto']);
-<<<<<<< HEAD
     $resultado = $comentarios->getComentarios($_GET['id'],$LIMIT);
     header("Content-Type: application/json; charset=UTF-8");
     sleep(1);
@@ -50,16 +34,12 @@ else {
   if ($_GET['action']=="obtener") {
 
     $resultado = $comentarios->getComentarios($_GET['id']);
-=======
-    $resultado = $comentarios->getComentarios($_GET['id'],$inicio,$limite);
->>>>>>> ab3e0b3f9bca0c53d4c1e0f1958dff10e5c482e3
     header("Content-Type: application/json; charset=UTF-8");
     sleep(1);
     echo $resultado;
   }
 
-<<<<<<< HEAD
-  if ($_GET['action']=="insertar") {
+if ($_GET['action']=="insertar") {
     $comentarios->insertarComentario($_GET['id'],$_SESSION['usuario'],$_GET['texto']);
     $resultado = $comentarios->getComentarios($_GET['id']);
     header("Content-Type: application/json; charset=UTF-8");
@@ -67,7 +47,6 @@ else {
     echo $resultado;
   }
 }
-=======
->>>>>>> ab3e0b3f9bca0c53d4c1e0f1958dff10e5c482e3
+
 
 ?>
